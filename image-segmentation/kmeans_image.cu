bcbd35f9312411867223b90ca20044fc6482067f
peterglezroa <peterglezroa@gmail.com>
2021-11-22 16:52:59 -0600

    kmeans image: v1.2

--- image-segmentation/kmeans_image.cu
+++ image-segmentation/kmeans_image.cu
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#include <curand.h>
+#include <curand_kernel.h>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/cuda.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -9,6 +14,32 @@
 
 #define TPB 512
 #define NLIMIT 100
+
+#define LOGS 1
+#define MAXDISPLAY 100
+
+/* Setup cura state */
+__global__
+void setupKernel(curandState *state){
+  int idx = threadIdx.x+blockDim.x*blockIdx.x;
+  curand_init(1234, idx, 0, &state[idx]);
+}
+
+/* Sets the centroid to a random point */
+__device__
+void randomPoint(curandState *state, const int clusID, const int dims,
+float *clusters, const int nelems, const float *elems) {
+    int cPos = clusID*dims;
+    // Generate random
+    // https://stackoverflow.com/questions/18501081/generating-random-number-within-cuda-kernel-in-a-varying-range
+    // uniform dist
+    float frand = curand_uniform(&(state[clusID]));
+    frand *= nelems-1;
+    int ePos = (int)truncf(frand);
+    ePos *= dims;
+    for (int i = 0; i < dims; i++)
+        clusters[cPos+i] = elems[ePos+1];
+}
 
 /* Function to group the elements in its nearest cluster.
  * Returns the distance to said cluster. */
@@ -45,13 +76,17 @@ float updateCentroid(const int clusID, const int dims, float *clusters,
 const int nelems, const float *elems, int *elemClus) {
     float avg, dis = 0;
     int cPos = clusID*dims;
+    int counter = 0;
 
     for (int i = 0; i < dims; i++) {
         avg = 0;
         for (int j = 0; j < nelems; j++)
-            if (elemClus[j] == clusID)
-                avg += elems[dims*j + i]/nelems;
-        dis += fabsf(clusters[cPos+1] - avg);
+            if (elemClus[j] == clusID) {
+                avg += elems[dims*j + i];
+                counter++;
+            }
+        avg /= counter;
+        dis += fabsf(clusters[cPos+i] - avg);
         clusters[cPos + i] = avg;
     }
 
@@ -90,12 +125,14 @@ int testChange(const int nclusters, const float *distances){
  *              iteration. size(float[epochs])
  */
 __global__
-void kmeans(const int dims, const int epochs, const int limit,
+void kmeans(curandState *state, const int dims, const int epochs, const int limit,
 const int nclusters, float *clusters, const int nelems, float *elems,
 int *elemClus, float *entropy, float *elemDis, float *movedDis, float *bC) {
     int tid = threadIdx.x + (blockIdx.x * blockDim.x);
 
     for (int i = 0; i < epochs; i++) {
+        if (tid < nclusters)
+            randomPoint(state+tid, tid, dims, clusters, nelems, elems);
         for (int j = 0; j < limit; j++) {
             // Calculate 
             if (tid < nelems)
@@ -133,10 +170,11 @@ int *elemClus, float *dst) {
 }
 
 int main(int argc, char *argv[]) {
-    cv::Mat og, src, dst, finalDst;
+    curandState *gpu_state;
+    cv::Mat og, tmp, dst, finalDst;
     int size, channels, nelems;
     int nclusters, epochs, limit;
-    float *dstRaw;
+    float *dstRaw, *src;
 
     float *gpu_src, *gpu_dst, *gpu_clusters, *gpu_entropy;
     int *gpu_elemClus;
@@ -172,10 +210,11 @@ int main(int argc, char *argv[]) {
     // Scan image and convert it to float
     fprintf(stdout, "Reading image...\n");
     og = cv::imread(argv[1], cv::IMREAD_COLOR);
-    og.convertTo(src, CV_32F);
-    nelems = src.rows * src.cols;
-    channels = src.channels();
-    size = src.rows * src.cols * og.channels();
+    og.convertTo(tmp, CV_32F);
+    src = (float*)tmp.data;
+    nelems = og.rows * og.cols;
+    channels = og.channels();
+    size = og.rows * og.cols * og.channels();
 
     // Allocate CPU
     fprintf(stdout, "Allocating memory in CPU...\n");
@@ -183,6 +222,7 @@ int main(int argc, char *argv[]) {
 
     // Copy to gpu
     fprintf(stdout, "Allocating memory in GPU...\n");
+    cudaMalloc(&gpu_state, sizeof(curandState));
     cudaMalloc((void**) &gpu_src, sizeof(float)*size);
     cudaMalloc((void**) &gpu_clusters, sizeof(float)*nclusters*channels);
     cudaMalloc((void**) &gpu_elemClus, sizeof(int)*nelems);
@@ -197,14 +237,62 @@ int main(int argc, char *argv[]) {
     cudaMalloc((void**) &gpu_bC, sizeof(float)*nclusters*channels);
 
     fprintf(stdout, "Uploading image to GPU...\n");
-    cudaMemcpy(gpu_src, src.data, sizeof(float)*size, cudaMemcpyHostToDevice);
+    cudaMemcpy(gpu_src, src, sizeof(float)*size, cudaMemcpyHostToDevice);
+
+    // Setup kernel
+    fprintf(stdout, "Setting up GPU kernel state...\n");
+    setupKernel<<<nelems/TPB+1, TPB>>>(gpu_state);
 
     // Call kmeans
     fprintf(stdout, "Applying kmeans...\n");
-    kmeans<<<nelems/TPB + 1, TPB>>>(channels, epochs, limit, nclusters,
+    kmeans<<<nelems/TPB + 1, TPB>>>(gpu_state, channels, epochs, limit, nclusters,
     gpu_clusters, nelems, gpu_src, gpu_elemClus, gpu_entropy,
     gpu_elemDis, gpu_movedDis, gpu_bC);
 
+    // Print logs
+    if(LOGS) {
+        float *clusters = (float *)malloc(sizeof(float)*nclusters*channels);
+        cudaMemcpy(clusters, gpu_clusters, sizeof(float)*nclusters*channels,
+            cudaMemcpyDeviceToHost);
+
+        float *entropy = (float *)malloc(sizeof(float)*epochs);
+        cudaMemcpy(entropy, gpu_entropy, sizeof(float)*epochs,
+            cudaMemcpyDeviceToHost);
+
+        float *elemDis = (float *)malloc(sizeof(float)*nelems);
+        cudaMemcpy(elemDis, gpu_elemDis, sizeof(float)*nelems,
+            cudaMemcpyDeviceToHost);
+
+        int *elemClus = (int *)malloc(sizeof(float)*nelems);
+        cudaMemcpy(elemClus, gpu_elemClus, sizeof(int)*nelems,
+            cudaMemcpyDeviceToHost);
+
+        fprintf(stdout, "Resulting clusters:\n");
+        // Log clusters
+        for (int i = 0; i < nclusters; i++) {
+            fprintf(stdout, "\t#%i: ", i);
+            for (int j = 0; j < channels; j++)
+                fprintf(stdout, "%.1f ", clusters[i*channels+j]);
+            fprintf(stdout, "\n");
+        }
+
+        fprintf(stdout, "\nElements:\n");
+        // Log elements and its distance
+        for (int i = 0; i < nelems && i < MAXDISPLAY; i++) {
+            fprintf(stdout, "\t#%i: ", i);
+            for (int j = 0; j < channels; j++)
+                fprintf(stdout, "%.1f ", src[i*channels+j]);
+            fprintf(stdout, "Cluster (%i, %.1f)\n", elemClus[i], elemDis[i]);
+        }
+
+        fprintf(stdout, "\nEntropies:\n");
+        for (int i = 0; i < epochs; i++)
+            fprintf(stdout, "\t%.3f", entropy[i]);
+
+        free(clusters); free(entropy); free(elemDis); free(elemClus);
+    }
+
+    /*
     // Call modified image
     fprintf(stdout, "Applying colors...\n");
     colorClusters<<<nelems/TPB + 1, TPB>>>(channels, gpu_clusters, nelems,
@@ -215,16 +303,17 @@ int main(int argc, char *argv[]) {
 
     // Convert result to opencv
     fprintf(stdout, "Obtaining image from GPU...\n");
-    dst = cv::Mat(src.rows, src.cols, src.type(), dstRaw, cv::Mat::AUTO_STEP);
+    dst = cv::Mat(og.rows, og.cols, og.type(), dstRaw, cv::Mat::AUTO_STEP);
     dst.convertTo(finalDst, CV_8U);
 
     // Display images
     fprintf(stdout, "Displaying images...\n");
-    cv::namedWindow("Original", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Original", og);
-    cv::namedWindow("GrayScale", cv::WINDOW_AUTOSIZE);
-    cv::imshow("GrayScale", dst);
-    cv::waitKey(0);
+//    cv::namedWindow("Original", cv::WINDOW_AUTOSIZE);
+//    cv::imshow("Original", og);
+//    cv::namedWindow("GrayScale", cv::WINDOW_AUTOSIZE);
+//    cv::imshow("GrayScale", dst);
+//    cv::waitKey(0);
+    */
 
     cudaFree(gpu_src); cudaFree(gpu_dst); cudaFree(gpu_clusters);
     cudaFree(gpu_elemClus); cudaFree(gpu_entropy);
